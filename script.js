4f7ddbd1f64a70afef4b56541f94bce780f0605a
egobeam <pavelraspaev@MacBook-Pro-Pavel.local>
2015-03-03 21:31:01 +0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('div').mouseenter(function() {
-		$('div').fadeTo('fast', 0.5)
+		$('div').fadeTo('fast', 1)
 	});
 	$('div').mouseleave(function() {
 		$('div').fadeOut('fast', 0.5)
