c598def5fdcb241da2a9a6abca6891c5bbb82b64
egobeam <pavelraspaev@MacBook-Pro-Pavel.local>
2015-03-03 22:22:15 +0400

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$('li').mouseenter(function() {
+	$('.menu').mouseenter(function() {
 		$('this').fadeTo('fast', 1)
 	});
-	$('li').mouseleave(function() {
+	$('.menu').mouseleave(function() {
 		$('this').fadeTo('fast', 0.5)
 	});
 });
